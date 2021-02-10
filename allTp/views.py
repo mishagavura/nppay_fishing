1030865095ed30ebceceb36f94a866d5b8596d3f
Misha Gavura <43016416+mishagavura@users.noreply.github.com>
2021-02-10 20:57:37 +0200

    Update views.py

--- allTp/views.py
+++ allTp/views.py
@@ -16,10 +16,10 @@ from django.core.mail import send_mail
 def login_in(request):
 	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 	if 1==1:
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
-			print(ip)
-        else:
-            ip = request.META.get('REMOTE_ADDR')
-            print(ip)
+	    if x_forwarded_for:
+		ip = x_forwarded_for.split(',')[0]
+		print(ip)
+	    else:
+		ip = request.META.get('REMOTE_ADDR')
+		print(ip)
 	return render(request, 'allTp/home.html')
